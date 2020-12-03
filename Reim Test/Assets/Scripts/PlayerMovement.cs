eb36b7aac536d7d2bb7cacb193adcac323116250
Ashley <ashley_roesler@baylor.edu>
2020-12-02 23:42:22 -0600

    we got movement :o

---
+++ Reim Test/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;      // player controller reference
+    public Transform cam;                       // camera reference
+
+    public float speed = 6f;                    // player speed
+
+    public float turnSmoothTime = 0.1f;         // smooths player rotation
+    float turnSmoothVelocity;                   // current smooth velocity
+
+    // Update is called once per frame
+    void Update()
+    {
+        // gather input information
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            // rotate player to make them look where they are going (based on camera rotation as well)
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            // move player based on input and camera rotation
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+    }
+}
