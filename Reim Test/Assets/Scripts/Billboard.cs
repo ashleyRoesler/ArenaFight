e3e6ed92484e66490db3f33d03c0a71c6cf97fa9
Ashley <ashley_roesler@baylor.edu>
2020-12-18 21:00:40 -0600

    health bars face local player, not owning player

--- Reim Test/Assets/Scripts/Billboard.cs
+++ Reim Test/Assets/Scripts/Billboard.cs
@@ -1,16 +1,26 @@
 ﻿using UnityEngine;
+using MLAPI;
 
-public class Billboard : MonoBehaviour
+public class Billboard : NetworkedBehaviour
 {
     private Transform cam;           // reference to camera location
+
+    private static Transform localCam;
 
     private void Start()
     {
         cam = gameObject.transform.parent.gameObject.GetComponent<PlayerController>().GetCamTransform();
+
+        if (IsLocalPlayer)
+        {
+            localCam = gameObject.transform.parent.gameObject.GetComponent<PlayerController>().GetCamTransform();
+
+        }
     }
 
     void LateUpdate()
     {
-        transform.LookAt(transform.position + cam.forward);
+        //    transform.LookAt(transform.position + cam.forward);
+        transform.LookAt(transform.position + localCam.forward);
     }
 }
